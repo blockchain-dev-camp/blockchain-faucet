00117a27fa12a0299009cbf7c67a7a52b9aafc49
frizeroBG <kovni@abv.bg>
2018-02-03 17:48:32 +0200

    repair faucet-controller.js -> postTransaction

--- controllers/faucet-controller.js
+++ controllers/faucet-controller.js
@@ -7,48 +7,21 @@ module.exports = {
     },
     postTransaction: (req, res) => {
         let faucetAddress = "39jzjt85tcRkqki6BeRzsD4FfF6BZKYQnR";
-        let faucetPublicKey = "";
-        let faucetPrivateKey = "";
-        let userAddres = req.body;
+        let faucetPublicKey = "2a1d79fb8743d0a4a8501e0028079bcf82a4eae1";
+        let faucetPrivateKey = "3a1f79fb8d743qd0ag4vrta850hhg1en00528gj0h79bfcf82ja4ea5eh";
+        let userAddres = req.body.address;
 
-        const http = require('http');
+        var request = require('request');
 
-        var postData = JSON.stringify({
+        let postData  = {
             "from": faucetAddress,
             "to": userAddres,
             "value": 5,
-            "senderPubKey": faucetPublicKey,
+            "senderPubKey": faucetPrivateKey,
             "senderSignature": ["e20c…a3c29d3370f79f", "cf92…0acd0c132ffe56"]
-        });
+          };
+        request.post('http://localhost:5555/transactions/new', {form:postData});
 
-        const options = {
-            hostname: 'http://http://localhost:',
-            port: 5555,
-            path: '/transactions/new',
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json',
-                'accept': 'application/json'
-            }
-        };
-
-        const request = http.request(options, (res) => {
-            res.setEncoding('utf8');
-            res.on('data', (chunk) => {
-                console.log(`BODY: ${chunk}`);
-            });
-            res.on('end', () => {
-                console.log('No more data in response.');
-            });
-        });
-
-        request.on('error', (e) => {
-            console.error(`problem with request: ${e.message}`);
-        });
-
-        // write data to request body
-        request.write(postData);
-        request.end();
         res.redirect('/');
     }
 };
